710206fb96340a3ea9add6da2f993691e8afbcae
g <you@example.com>
2025-04-21 21:16:01 +0200

    static production issue

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 /**
  * @type {import('next').NextConfig}
  */
+const repoName = 'bineural-player'; // Replace with your repository name
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig = {
   output: 'export',
- 
+  basePath: isGithubActions ? `/${repoName}` : '',
+  // assetPrefix: isGithubActions ? `/${repoName}/` : '', // Removed assetPrefix
+
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
  
