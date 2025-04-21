9085543cc4ac4eda0f3a9e9d9d6d578ac2d7ccf5
g <you@example.com>
2025-04-21 23:22:17 +0200

    gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 /**
  * @type {import('next').NextConfig}
  */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   output: 'export',
- 
+  // Set the base path and asset prefix for GitHub Pages deployment
+  basePath: isProd ? '/bineural-player' : undefined,
+  assetPrefix: isProd ? '/bineural-player/' : undefined, // assetPrefix requires a trailing slash
+
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
  
@@ -13,5 +18,5 @@ const nextConfig = {
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
 }
- 
+
 module.exports = nextConfig
