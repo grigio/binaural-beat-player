44c05371d61d347c91a24809cb4a398027e51d7f
g <you@example.com>
2025-04-22 13:44:08 +0200

    renamed

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   output: 'export',
   // Set the base path and asset prefix for GitHub Pages deployment
-  basePath: isProd ? '/bineural-player' : undefined,
-  assetPrefix: isProd ? '/bineural-player/' : undefined, // assetPrefix requires a trailing slash
+  basePath: isProd ? '/binaural-beat-player' : undefined,
+  assetPrefix: isProd ? '/binaural-beat-player/' : undefined, // assetPrefix requires a trailing slash
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
